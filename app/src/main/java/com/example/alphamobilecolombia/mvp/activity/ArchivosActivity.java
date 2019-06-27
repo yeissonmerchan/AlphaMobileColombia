7c59b6bf8414827e312993c4d90e09e83d4eba0f
yeisson.merchan <yeisson.merchan@alphacredit.co>
2019-06-27 12:03:54 -0500

    #ajuste documentos

--- app/src/main/java/com/example/alphamobilecolombia/mvp/activity/ArchivosActivity.java
+++ app/src/main/java/com/example/alphamobilecolombia/mvp/activity/ArchivosActivity.java
@@ -1,5 +1,7 @@
 package com.example.alphamobilecolombia.mvp.activity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,6 +14,7 @@ import com.example.alphamobilecolombia.utils.models.Person;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
@@ -32,12 +35,16 @@ import android.widget.TextView;
 import com.example.alphamobilecolombia.R;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArchivosActivity extends AppCompatActivity {
     private ImageView imagen1;
     Spinner spinner_tipo_documento;
     RealmStorage storage = new RealmStorage();
     View view;
+    Context context;
+    List<com.example.alphamobilecolombia.utils.models.File> listUpload = new ArrayList<com.example.alphamobilecolombia.utils.models.File>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,13 +72,19 @@ public class ArchivosActivity extends AppCompatActivity {
 
 
     public void tomarFoto(View v) {
+        boolean existFile = existDocumentUpload();
         view = v;
-        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
-        StrictMode.setVmPolicy(builder.build());
-        Intent intento1 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File foto = new File(getExternalFilesDir(null), getNameFile(v));
-        intento1.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(foto));
-        startActivityForResult(intento1,1);
+        if (!existFile){
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+            StrictMode.setVmPolicy(builder.build());
+            Intent intento1 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            File foto = new File(getExternalFilesDir(null), getNameFile(v));
+            intento1.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(foto));
+            startActivityForResult(intento1,1);
+        }
+        else{
+            NotificacionExistente(v);
+        }
     }
 
     public void recuperarFoto(View v) {
@@ -95,5 +108,68 @@ public class ArchivosActivity extends AppCompatActivity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         recuperarFoto(view);
+        ConfirmacionImagen(view);
+    }
+
+    public void finalizacion(View view) {
+        Intent intento1=new Intent(this,FinalActivity.class);
+        startActivity(intento1);
+    }
+
+    public boolean existDocumentUpload(){
+        final String nameFile = spinner_tipo_documento.getSelectedItem().toString();
+        boolean result = false;
+
+        for(com.example.alphamobilecolombia.utils.models.File file : listUpload) {
+            if(file.getType().equals(nameFile)) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public void ConfirmacionImagen(final View view){
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(view.getContext());
+        builder1.setMessage("¿ Deseas guardar esta imagen ?");
+        builder1.setCancelable(true);
+        final String nameFile = spinner_tipo_documento.getSelectedItem().toString();
+        builder1.setPositiveButton(
+                "Sí",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        listUpload.add(new com.example.alphamobilecolombia.utils.models.File(0, getNameFile(view),false, nameFile,true));
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        Bitmap bitmap1 = BitmapFactory.decodeFile(null);
+                        imagen1.setImageBitmap(bitmap1);
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+
+    public void NotificacionExistente(final View view){
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(view.getContext());
+        builder1.setMessage("Este archivo ya esta cargado, por favor selecione otro.");
+        builder1.setCancelable(true);
+        builder1.setPositiveButton(
+                "Ok",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
---
+++ app/src/main/java/com/example/alphamobilecolombia/utils/models/File.java
@@ -0,0 +1,78 @@
+package com.example.alphamobilecolombia.utils.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class File {
+    private int IdType;
+    private String Type;
+    private String Name;
+    private boolean IsRequired;
+
+    public boolean isUpload() {
+        return IsUpload;
+    }
+
+    public void setUpload(boolean upload) {
+        IsUpload = upload;
+    }
+
+    private boolean IsUpload = false;
+
+    public File(int setIdType, String setName, boolean setRequired, String setType, boolean setUpload) {
+        IdType = setIdType;
+        Name = setName;
+        IsRequired = setRequired;
+        Type = setType;
+        IsUpload = setUpload;
+    }
+
+    public int getIdType() {
+        return IdType;
+    }
+
+    public void setIdType(int idType) {
+        IdType = idType;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public boolean isRequired() {
+        return IsRequired;
+    }
+
+    public void setRequired(boolean required) {
+        IsRequired = required;
+    }
+
+    public List<File> getFiles(){
+        List<File> list = new ArrayList<File>();
+        list.add(new File(66,"",true,"SolicitudCreditoCara1",false));
+        list.add(new File(67,"",true,"SolicitudCreditoCara2",false));
+        list.add(new File(68,"",true,"CedulaCara1",false));
+        list.add(new File(69,"",true,"CedulaCara2",false));
+        list.add(new File(70,"",true,"Desprendible1",false));
+        list.add(new File(71,"",true,"Desprendible2",false));
+        list.add(new File(72,"",false,"Desprendible3",false));
+        list.add(new File(73,"",false,"Desprendible4",false));
+        list.add(new File(74,"",false,"FormatoFirmaRuego",false));
+        list.add(new File(75,"",false,"FormatoFirmaRuego",false));
+        list.add(new File(76,"",false,"CedulaTestigoFirmaRuego",false));
+
+        return list;
+    }
+}
