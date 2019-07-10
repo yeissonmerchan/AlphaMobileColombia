a5e9a7c094bef9e45daf2a918d0dc75ba305d452
cristian.ruiz <cristian.ruiz@alphacredit.co>
2019-07-10 17:54:22 -0500

    Ajustes visuales app bar

--- app/src/main/java/com/example/alphamobilecolombia/mvp/activity/ConsultaCreditosActivity.java
+++ app/src/main/java/com/example/alphamobilecolombia/mvp/activity/ConsultaCreditosActivity.java
@@ -52,6 +52,9 @@ public class ConsultaCreditosActivity extends AppCompatActivity {
 
         Window window = this.getWindow();
 
+        TextView modulo = findViewById(R.id.txt_modulo);
+        modulo.setText("Mis solicitudes");
+
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
