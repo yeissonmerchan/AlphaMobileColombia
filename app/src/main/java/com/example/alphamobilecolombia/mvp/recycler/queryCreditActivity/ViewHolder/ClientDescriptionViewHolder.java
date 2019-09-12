c3b39765a130ecf9024bd2d087f6fb4f7bc48287
caroline.lenes <carolain.lenes@alphacredit.co>
2019-09-12 17:47:11 -0500

    validacion de los campos a mostrar dependiendo de la respuesta del servicio.

--- app/src/main/java/com/example/alphamobilecolombia/mvp/recycler/queryCreditActivity/ViewHolder/ClientDescriptionViewHolder.java
+++ app/src/main/java/com/example/alphamobilecolombia/mvp/recycler/queryCreditActivity/ViewHolder/ClientDescriptionViewHolder.java
@@ -5,12 +5,22 @@ import android.widget.TextView;
 
 import com.example.alphamobilecolombia.R;
 import com.example.alphamobilecolombia.data.remote.Models.Response.PostQueryCredit;
-import com.example.alphamobilecolombia.mvp.recycler.queryCreditActivity.Model.ClientDescription;
 import com.thoughtbot.expandablerecyclerview.viewholders.ChildViewHolder;
 
 import java.text.DecimalFormat;
 
 public class ClientDescriptionViewHolder extends ChildViewHolder {
+
+    //Fileds
+    private TextView fieldEstado;
+    private TextView fieldPagaduria;
+    private TextView fieldFechaEnvio;
+    private TextView fieldMontoSugerido;
+    private TextView fieldCuotaSugerida;
+    private TextView fieldPlazoSugerido;
+    private TextView fieldFechaPrevalidacion;
+    private TextView fieldObsPrevalidacion;
+    private TextView fieldObsCredito;
 
     private TextView estadoText;
     private TextView pagaduriaText;
@@ -23,8 +33,19 @@ public class ClientDescriptionViewHolder extends ChildViewHolder {
     private TextView observacionCreditoText;
     private DecimalFormat format = new DecimalFormat("$###,###.##");
 
+
     public ClientDescriptionViewHolder(View itemView) {
         super(itemView);
+        //Fields
+        fieldEstado = itemView.findViewById(R.id.fieldEstado);
+        fieldPagaduria = itemView.findViewById(R.id.fieldPagaduria);
+        fieldFechaEnvio = itemView.findViewById(R.id.fieldFechaEnvio);
+        fieldMontoSugerido = itemView.findViewById(R.id.fieldMontoSugerido);
+        fieldCuotaSugerida = itemView.findViewById(R.id.fieldCuotaSugerida);
+        fieldPlazoSugerido = itemView.findViewById(R.id.fieldPlazoSugerido);
+        fieldFechaPrevalidacion = itemView.findViewById(R.id.fieldFechaPrevalidacion);
+        fieldObsPrevalidacion = itemView.findViewById(R.id.fieldObsPrevalidacion);
+        fieldObsCredito = itemView.findViewById(R.id.fieldObsCredito);
 
         estadoText = itemView.findViewById(R.id.estadoText);
         pagaduriaText = itemView.findViewById(R.id.pagaduriaText);
@@ -41,34 +62,63 @@ public class ClientDescriptionViewHolder extends ChildViewHolder {
 
         String estado = creditDescription.getGeneralState();
         if (estado.equals("")){
-            estadoText.setVisibility(View.GONE);
+            fieldEstado.setVisibility(View.GONE);
         }else{
             estadoText.setText(estado);
         }
 
-        pagaduriaText.setText(creditDescription.getPagaduria());
-        fechaEnvioPrevaText.setText(creditDescription.getFechaEnvioPrevalidacion());
-        montoSugeridoText.setText(format.format(Integer.parseInt(creditDescription.getMontoSugerido())));
-        cuotaSugText.setText(format.format(Integer.parseInt(creditDescription.getCuotaSug())));
-        plazoSugeridoText.setText(String.format("%1s Meses", creditDescription.getPlazoSugerido()));
-        fechaPrevalidacionText.setText(creditDescription.getFechaPrevalidacion());
-        //observacionPrevaText.setText(creditDescription.getObsPrevalidacion());
-        //observacionCreditoText.setText(creditDescription.getObservacionCredito());
+        if (creditDescription.getPagaduria().equals("")){
+            fieldPagaduria.setVisibility(View.GONE);
+        }else{
+            pagaduriaText.setText(creditDescription.getPagaduria());
+        }
 
+        if (creditDescription.getFechaEnvioPrevalidacion().equals("")){
+            fieldFechaEnvio.setVisibility(View.GONE);
+        }else{
+            fechaEnvioPrevaText.setText(creditDescription.getFechaEnvioPrevalidacion());
+        }
+
+        String montoSugerido = format.format(Integer.parseInt(creditDescription.getMontoSugerido()));
+        if (montoSugerido.equals("")){
+            fieldMontoSugerido.setVisibility(View.GONE);
+        }else{
+            montoSugeridoText.setText(montoSugerido);
+        }
+
+        String cuotaSugerida = format.format(Integer.parseInt(creditDescription.getCuotaSug()));
+        if (cuotaSugerida.equals("")){
+            fieldCuotaSugerida.setVisibility(View.GONE);
+        }else{
+            cuotaSugText.setText(cuotaSugerida);
+        }
+
+        String plazoSugerido = String.format("%1s Meses", creditDescription.getPlazoSugerido());
+        if (plazoSugerido.equals("")){
+            fieldPlazoSugerido.setVisibility(View.GONE);
+        }else{
+            plazoSugeridoText.setText(plazoSugerido);
+        }
+
+        if (creditDescription.getFechaPrevalidacion().equals("")){
+            fieldFechaPrevalidacion.setVisibility(View.GONE);
+        }else{
+            fechaPrevalidacionText.setText(creditDescription.getFechaPrevalidacion());
+        }
 
 
         String obsPreva = creditDescription.getObsPrevalidacion();
-        if (obsPreva.equals("")){
-            observacionPrevaText.setVisibility(View.GONE);
+        if (obsPreva.equals("") || obsPreva == null){
+            fieldObsPrevalidacion.setVisibility(View.GONE);
         }else{
             observacionPrevaText.setText(obsPreva);
         }
 
-        String obsCredrit = creditDescription.getObservacionCredito();
+       String obsCredrit = creditDescription.getObservacionCredito();
         if (obsCredrit !=null){
             observacionCreditoText.setText(obsCredrit);
         }else{
-            observacionCreditoText.setVisibility(View.GONE);
+            fieldObsCredito.setVisibility(View.GONE);
         }
     }
 
