979979586d2db702380310bb976e8892a5738c14
yeisson.merchan <yeisson.merchan@alphacredit.co>
2019-09-18 09:33:19 -0500

    actualización ambiente

--- app/src/main/java/com/example/alphamobilecolombia/utils/validaciones/Formulario.java
+++ app/src/main/java/com/example/alphamobilecolombia/utils/validaciones/Formulario.java
@@ -84,7 +84,12 @@ public class Formulario {
                     //Si el control x es Spinner entonces
                 } else if (Control instanceof Spinner && ((Spinner) Control).getSelectedItem() != null) {
                     //Texto = ((Spinner) Control).getSelectedItem().toString();
-                    Texto = String.valueOf(_iSelectList.GetValueByCodeField(((Spinner) Control).getSelectedItem().toString()));
+                    if(arrayId[Indice] == "search_tipo_contrato" && Texto.trim().equals("")){
+                        Texto = "15";
+                    }
+                    else{
+                        Texto = String.valueOf(_iSelectList.GetValueByCodeField(((Spinner) Control).getSelectedItem().toString()));
+                    }
                 } else if (Control instanceof SearchView) {
                     SearchView searchView = (SearchView)Control;
                     Texto = searchView.getQuery().toString();
